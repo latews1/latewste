d817e6a8ae1ad370a704917c60b42e778e61c0d7
latews1 <118557232+latews1@users.noreply.github.com>
2023-03-11 02:02:37 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -1451,18 +1451,7 @@ class SendSms():
                     break
             else:
                 continue
-    
-    def snap(phone):
-        liste = [self.phone, self.phone2, self.phone3, self.phone4, self.phone5]
-        bos_olmayan = len([x for x in liste if x != "bos"])
-        uygulanan_nolar = 0
-        snapH = {"Host": "app.snapp.taxi", "content-length": "29", "x-app-name": "passenger-pwa", "x-app-version": "5.0.0", "app-version": "pwa", "user-agent": generate_user_agent(os="android"), "content-type": "application/json", "accept": "*/*","origin": "https://app.snapp.taxi", "sec-fetch-site": "same-origin", "sec-fetch-mode": "cors", "sec-fetch-dest": "empty", "referer": "https://app.snapp.taxi/login/?redirect_to\u003d%2F", "accept-encoding": "gzip, deflate, br", "accept-language": "fa-IR,fa;q\u003d0.9,en-GB;q\u003d0.8,en;q\u003d0.7,en-US;q\u003d0.6", "cookie": "_gat\u003d1"}
-        snapD = {"cellphone": phone}
-        try:
-            post(url="https://app.snapp.taxi/api/api-passenger-oauth/v2/otp", headers=snapH, json=snapD)
-            return True
-        except:
-            pass
+
 
     def filmnet(phone):
         fnU = f"https://api-v2.filmnet.ir/access-token/users/{phone}/otp"
